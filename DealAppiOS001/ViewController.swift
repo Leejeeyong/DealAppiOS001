e01efdb7a261d94095475aec62f344f5c53f6da4
leejeeyong <fghj0720@gmail.com>
2020-09-14 17:10:34 +0900

    first commit

--- DealAppiOS001/ViewController.swift
+++ DealAppiOS001/ViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let myCell = tableView.dequeueReusableCell(withIdentifier: "HomeCell", for: indexPath)
+        
+    }
+    
+    class tableViewCell: UITableViewCell{
+        @IBOutlet weak var imageviewCell: UIImageView!
+        @IBOutlet weak var titleLabelCell: UILabel!
+        
+    }
 }
 
